67099a097897829a57391d16479f315ba65b17e7
Junior627 <junior62725@gmail.com>
2019-11-06 23:31:18 +0000

    iglatinpay

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#imp").click(function(){
+  let pigLatin =$("#in").val();
+  let parray= pigLatin.split("");
+  console.log(parray);
+  let firstLetter= parray[0];
+  console.log(firstLetter);
+
+  let vowel= ["a","e","i","o","u"];
+    if(vowel.includes(firstLetter)){
+      pigLatin=pigLatin+"ay";
+    }
+    else{
+      function isVowel (vowe){
+        return vowel.includes(vowe)
+      }
+      parray.findIndex(vowe);
+      let igLatin=pigLatin.substr(1);
+      pigLatin=igLatin+firstLetter+"ay";
+    }
+    $("#result").text(pigLatin);
+});
 
 
 
-  
-  
-  
-  
+
+
+
+
+
 });
